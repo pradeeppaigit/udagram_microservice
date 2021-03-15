c068a4f3f892c772fde6d76ee7880ed232ed8398
Pradeep Pai <mailpradeeppai@gmail.com>
2021-03-14 22:40:12 -0700

    2nd attempt after updating URLs

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a5569d3e190e4452ca4a20cf5330f216-1792448961.us-east-1.elb.amazonaws.com:8080/api/v0'//udagram-reverseproxy External IP 
+  //'http://localhost:8080/api/v0'
 };
 
 /*
